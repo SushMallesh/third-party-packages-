6e3cefb70baa74800c90ba208b50bbba36d95b5b
Sushma <sushma30191@gmail.com>
2022-12-04 14:44:18 +0000

    video and charts third party packages

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import VideoPlayer from './components/VideoPlayer'
+import LineChartInfo from './components/LineChartInfo'
+import BarChartInfo from './components/BarChartInfo'
+import PieChartInfo from './components/PieChartInfo'
+import AreaChartInfo from './components/AreaChartInfo'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <VideoPlayer />
+    <LineChartInfo />
+    <BarChartInfo />
+    <AreaChartInfo />
+    <PieChartInfo />
+  </>
+)
+
+export default App
